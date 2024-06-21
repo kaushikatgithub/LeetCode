17f4ee145afb82c77d1a97445615791c959edbd3
Kaushik Prajapati <122290025+kaushikatgithub@users.noreply.github.com>
2024-06-21 19:30:20 +0530

    Create 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp

---
+++ Medium/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int solve(vector<int> &p, int bought, int cday, vector<vector<int>> &dp)
+    {
+        if(p.size() <= cday)
+        {
+            if(bought == 0) return 0;
+            else return -1e8;
+        }
+
+        if(dp[cday][bought] != -1) return dp[cday][bought];
+
+        int ans = 0;
+        if(bought)
+        {
+            ans = max(solve(p, bought, cday+1, dp), p[cday] + solve(p, 0, cday+2, dp)); 
+        }else
+        {   
+            ans = max(solve(p, bought, cday+1, dp), -p[cday] + solve(p, 1, cday+1, dp));
+        }
+        return dp[cday][bought] = ans;
+    }
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n+1, vector<int> (3, -1));
+        return solve(p, 0, 0, dp);
+    }
+};
